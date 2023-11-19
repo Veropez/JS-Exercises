51051a8dca9a4cc56327f0aa0cbc7e1ffbf94cca
Veropez <veronicalopa@gmail.com>
2023-11-19 03:49:02 +0100

    actualizacion

---
+++ S3/2.dom-modify/exercise-5.js
@@ -0,0 +1,5 @@
+const title$$ = document.querySelector('h2.fn-insert-here');
+title$$.textContent = 'Wubba Lubba dub dub';
+
+
+//document.body.appendChild(title$$); Repasar grabación no hay que añadirlo porque ya está el nodo .fn-insert-here
---
+++ S3/2.dom-modify/exercise-6.js
@@ -0,0 +1,10 @@
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const list$$ = document.createElement('ul');
+
+for(const app of apps) {
+    const item$$ = document.createElement('li');
+    item$$.textContent = app;
+    list$$.appendChild(item$$);
+}
+
+document.body.appendChild(list$$);
